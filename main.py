136a07ce8a68e3f085efd1cc920f5f1fd93cd866
Dima_Home <dim.zam1986@gmail.com>
2026-05-31 19:08:33 +0300

    Add style for FastApi

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from generate import generate_contract
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="web"), name="static")
 
 @app.get("/")
 async def root():
-    return FileResponse("index.html")
-
+    return FileResponse("web/index.html")
 
 @app.post("/submit")
 async def submit_form(
